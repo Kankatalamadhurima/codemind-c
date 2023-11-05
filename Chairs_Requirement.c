bd892482f9f36a520401153a08212b8ed48d013d
Kankatalamadhurima <147476027+Kankatalamadhurima@users.noreply.github.com>
2023-11-05 22:57:07 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);     z=x-y;     if(x>=y)     {         printf("%d",z);     }     else     {         printf("%d",z);     } }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=x-y;
+    if(x>=y)
+    {
+        printf("%d",z);
+    }
+    else
+    {
+        printf("%d",z);
+    }
+}
